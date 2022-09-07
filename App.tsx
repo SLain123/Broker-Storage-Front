3c6fc5dd07ae00d7926df1969228a835c4c7978d
Artem <sl163@ya.ru>
2022-09-07 16:58:02 +0300

    Fixes bug with unavailable redux package;

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Navigation } from 'modules/navigation/Navigation';
 import { Register } from 'screens/Register';
-import { store } from 'redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 export default function App() {
