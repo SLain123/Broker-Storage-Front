93a9d9fa5155fc9d0185aa7fe2ffa06b6f34cd14
Artem <sl163@ya.ru>
2022-10-25 12:40:04 +0300

    Adds button for exit from account on pin code screen;

--- modules/auth-pin/AuthPin.tsx
+++ modules/auth-pin/AuthPin.tsx
@@ -1,10 +1,11 @@
 import React, { useState, FC, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 import SimplePin from 'react-native-simple-pin';
 import Toast from 'react-native-root-toast';
 
 import { useAppSelector, useAppDispatch } from 'hooks';
-import { setPin, getPin } from 'slice/authSlice';
+import { setPin, getPin, setAuthStatus } from 'slice/authSlice';
+import { saveToStore } from 'utils/secureStoreFuncs';
 
 const AuthPin: FC = () => {
     const [resultMessage, setResultMessage] = useState('');
@@ -23,6 +24,12 @@ const AuthPin: FC = () => {
         setResultMessage('PIN Code Wrong');
     };
 
+    const exitFromAccount = () => {
+        saveToStore('token', '');
+        dispatch(setAuthStatus(false));
+        dispatch(setPin([]));
+    };
+
     useEffect(() => {
         resultMessage &&
             Toast.show(resultMessage, {
@@ -36,6 +43,13 @@ const AuthPin: FC = () => {
 
     return (
         <View style={styles.container}>
+            <TouchableOpacity
+                activeOpacity={0.5}
+                style={styles.exitBtn}
+                onPress={exitFromAccount}
+            >
+                <Text style={styles.exitText}>Exit from Account</Text>
+            </TouchableOpacity>
             <SimplePin
                 pin={pin}
                 title='Type your PIN'
@@ -60,6 +74,16 @@ const styles = StyleSheet.create({
     },
     numPad: { fontSize: 30, color: 'white' },
     bullet: { fontSize: 30, color: '#2756B1', margin: 16 },
+    exitBtn: {
+        backgroundColor: '#2756B1',
+        padding: 16,
+        width: 240,
+        borderRadius: 4,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 24,
+    },
+    exitText: { color: 'white', textAlign: 'center' },
 });
 
 export { AuthPin };
