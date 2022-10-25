c39143388899ac5568cd28c512d0a6e164b405d7
Artem <sl163@ya.ru>
2022-10-25 11:33:22 +0300

    Removes unnecessary toast wrapper in App;

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
-import { RootSiblingParent } from 'react-native-root-siblings';
 
 import { Navigation } from 'modules/navigation/Navigation';
 import { AuthModule } from 'modules/auth/AuthModule';
@@ -10,10 +9,8 @@ import { AuthModule } from 'modules/auth/AuthModule';
 export default function App() {
     return (
         <Provider store={store}>
-            <RootSiblingParent>
-                <AuthModule />
-                <Navigation />
-            </RootSiblingParent>
+            <AuthModule />
+            <Navigation />
             <StatusBar />
         </Provider>
     );
